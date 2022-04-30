32f7f48608de05ea636edcc8c92b86fb9f21260c
emmarrat <99384076+emmarrat@users.noreply.github.com>
2022-04-30 12:32:03 +0500

    Update smart.php

--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -13,12 +13,12 @@ $mail->CharSet = 'utf-8';
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'emmarratt@gmail.com';                 // Наш логин
-$mail->Password = 'Mokotoshka281099!@';                           // Наш пароль от ящика
+$mail->Username = '';                 // Наш логин
+$mail->Password = '';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
-$mail->setFrom('emmarratt@gmail.com', 'Pulse');   // От кого письмо 
+$mail->setFrom('', 'Pulse');   // От кого письмо 
 $mail->addAddress('xexawoc157@flowminer.com');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
